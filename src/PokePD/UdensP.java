02dc98001f8031669cf188ce90b9f3ae5cdf573b
Sginats <Senvaldsniks2008@gmail.com>
2025-11-07 09:12:10 +0200

    Programma uzsakta

---
+++ src/PokePD/UdensP.java
@@ -0,0 +1,18 @@
+package PokePD;
+/*/
+Izstrādāt objektorientetas programmas risinājumu par temu „Pokemoni”. Prasībās ir noteikts, ka programma satur vismaz četras klases (nepieciešamības gadījumā, veidot papildus klases) Pokemons, ElektriskaisP, UdensP un Pokedatnis.
+Klase Pokedatnis satur programmas main daļu, no tās veic jaunu Pokemonu izveidi, atributu aplūkošanu, metožu izsaukšanu, cīņas uzsākšanu, tiek nodrošinātas darbības ar jebkura tipa Pokemona objektiem.
+Pokemons ir abstrakta bāzes klase, savukārt klases ElektriskaisP un UdensP ir apakšklases, kuras interprete jebkuram Pokemonam raksturīgās metodes.
+Programmai ir jāatbilst sekojošäm prasïbäm:
+»	klasēs ieviesti vismaz 4 atribūti un 4 metodes (vismaz víena no bāzes klases metodēm ír abstrakta)
+•	Pokemons ir abstrakta klase un strādājot ar apakškalsem, tiek ieveroti polimorfisma pamatprincipi
+•	tiek nodrošināta funkcionalitāte jauna Pokemona țElektriskaisP, UdensP) izveidei un uzglabāšanai
+•		tiek nodrošināta iespēja izsaukt metodes izveletajam pokemona objektam (uzbrukt, izvairīties, dziedēt, attístīt, utml.)
+»	ieviesta funkcionalitāte pokemonu turnīra rïkošanai, nodrošinot iespēju tiem savā starpā sacensties
+•	maksimäli noversta nekorektu datu ievade un iespejamie kļudu gadījumi.
+Kā papildus pluss tiks uzskatīta lietotāja grafiskä interfeisa izveide íformas, pogas, skanas u.tml).
+
+ */
+public class UdensP {
+
+}
